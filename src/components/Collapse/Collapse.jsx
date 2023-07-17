78300f1b68606864397e25838d3b72cec0e0e45a
Jeremy_lem <jeremy.lemaignent@gmail.com>
2023-07-17 14:27:57 +0200

    debug transition

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -11,30 +11,11 @@ const Collapse = ({ title, content, className }) => {
 
     return (
         <article className={className}>
-            <h4 onClick={handleClick} className={`collapse ${collapseClass}`}>
+            <h4 onClick={handleClick} className="collapse">
                 {title}
-                <i
-                    className={`fas fa-chevron-up`}
-                    style={{
-                        transformOrigin: "center",
-                        transform: `rotate(${
-                            isCollapsed ? "0deg" : "-180deg"
-                        })`,
-                        transition: "all 0.3s ease-in-out",
-                    }}
-                ></i>
+                <i className={`fas fa-chevron-up ${collapseClass}`}></i>
             </h4>
-            <div
-                className={`collapse-content ${collapseClass}`}
-                style={{
-                    maxHeight: isCollapsed ? "0" : "1000px",
-                    transform: `scaleY(${isCollapsed ? 0 : 1})`,
-                    transformOrigin: "top",
-                    transition: "all 0.3s ease-in-out",
-                }}
-            >
-                {content}
-            </div>
+            <div className={`collapse-content ${collapseClass}`}>{content}</div>
         </article>
     );
 };
